ce4fe59e1232da8e617f03df66e28d064ffd943a
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-06-18 19:43:12 -0500

    Actualiza ejemplo.cpp : Imprime el empleado que vendió más en todo el año

--- Ejemplo.cpp
+++ Ejemplo.cpp
@@ -1,49 +1,56 @@
 #include <iostream>
 using namespace std;
 
-struct EMP
-{
+struct EMP{
     int num;
     string nom;
     float ven[12], salario;
 };
 
-int main()
-{
+int main(){
     EMP EMPLEADOS[100];
-    int n;
+    int n, aux, num_mayor;
+    string nom_mayor;
 
     cout << "Ingrese el numero de empleados:" << endl;
     cin >> n;
+    int suma[n];
 
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         cout << "Numero del empleado: ";
         cin >> EMPLEADOS[i].num;
         cout << "Ingrese el nombre del empleado: ";
         cin >> EMPLEADOS[i].nom;
-        for (int j = 0; j < 12; j++)
-        {
-            cout << "Venta Nro " << j + 1 << ": ";
+        for (int j = 0; j < 12; j++){
+            cout << "Venta del Mes Nro " << j + 1 << ": ";
             cin >> EMPLEADOS[i].ven[j];
+            suma[i] = suma[i] + EMPLEADOS[i].ven[j];
         }
         cout << "Ingrese el salario bruto de este empleado: ";
         cin >> EMPLEADOS[i].salario;
         cout << "\n";
     }
+    
+    aux = suma[0];
+    for (int i=0; i<n; i++){
+        if (suma[i] > aux){
+            aux = suma[i];
+            num_mayor = i+1;
+            nom_mayor = EMPLEADOS[i].nom;
+        }
+    }
 
     cout << "LISTA COMPLETA:" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << "Numero del empleado: " << EMPLEADOS[i].num << endl;
+    for (int i = 0; i < n; i++){
+        cout << "Empleado Nro: " << EMPLEADOS[i].num << endl;
         cout << EMPLEADOS[i].nom << endl;
-        for (int j = 0; j < 12; j++)
-        {
-            cout << "Venta Nro " << j + 1 << ": " << EMPLEADOS[i].ven[j] << endl;
-        }
         cout << "Salario Neto: " << EMPLEADOS[i].salario << endl;
         cout << "\n";
     }
+
+    cout << "Empleado con mayores ventas anuales:" << endl;
+    cout << "Numero: " << num_mayor << endl;
+    cout << "Nombre: " << nom_mayor << endl;
 
     return 0;
 }
