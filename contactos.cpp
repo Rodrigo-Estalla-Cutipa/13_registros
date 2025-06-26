fe76458bd265c94f787248bea6f3fa30fb825d54
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-06-25 23:24:19 -0500

    AGREGA actualizacion de contactos.cpp: Opción de MODIFICAR contactos ya agregados

--- contactos.cpp
+++ contactos.cpp
@@ -17,7 +17,7 @@ void leerCorreo(correo &, string, string);
 void leerContacto(contactoEmail &, string, char, int, correo);
 void imprimeContacto(contactoEmail &);
 
-void modificaContacto(int, contactoEmail &);
+void modificaContacto(contactoEmail &);
 
 int main(){
     int n, op;
@@ -49,7 +49,7 @@ int main(){
                 cout<<"Ingrese el nombre del contacto: "; getline(cin,nom);
                 cout<<"Ingrese el sexo (M/F): "; cin>>sex;
                 cout<<"Ingrese la edad: "; cin>>edad;
-                cout<<"Ingrese el correo electronico (usuario@dominio): "<<endl;
+                cout<<"Ingrese el correo electronico (usuario@dominio):"<<endl;
                 cout<<"\tIngrese el usuario: "; cin>>user;
                 cout<<"\tIngrese el dominio: "; cin>>domain;
                 
@@ -67,7 +67,7 @@ int main(){
                     cout<<endl;
                 }
                 cout<<"QUE CONTACTO DESEA MODIFICAR?: "; cin >> modificar;
-                modificaContacto(modificar, lista[modificar-1]);
+                modificaContacto(lista[modificar-1]);
                 break;
             case 3:
                 for(int i = 0; i < n; i++){
@@ -78,8 +78,8 @@ int main(){
                 system("pause");
                 break;
             case 0:
-                cout<<"Esta seguro de salir? (Y/n): "; cin >> salir;
-                if (salir == 'n'){
+                cout<<"Esta seguro de salir? (S/N): "; cin >> salir;
+                if (salir == 'N'){
                     op = 3;
                 }
                 break;
@@ -93,10 +93,63 @@ int main(){
     return 0;
 }
 
-void modificaContacto(int aux, contactoEmail &){
-    cout << "Indique el apartado que desea modificar:" << endl;
-}
+void modificaContacto(contactoEmail &cambio){
+    string n, user, domain;
+    char s;
+    int e;
+    correo email;
 
+    n = cambio.nom;
+    s = cambio.sex;
+    e = cambio.edad;
+
+    email = cambio.email;
+    user = email.user;
+    domain = email.domain;
+
+    char aux;
+
+    cout << "Modificar nombre? (S/N): ";
+    cin >> aux;
+    if (aux == 'S'){
+        cin.ignore();
+        cout << "\tIngrese nombre: ";
+        getline(cin,n);
+    }
+    cout << "Modificar sexo? (S/N): ";
+    cin >> aux;
+    if (aux == 'S'){
+        cout << "\tIngrese sexo (M/F): ";
+        cin >> s;
+    }
+    cout << "Modificar edad? (S/N): ";
+    cin >> aux;
+    if (aux == 'S'){
+        cout << "\tIngrese edad: ";
+        cin >> e;
+    }
+    cout << "Modificar correo? (S/N): ";
+    cin >> aux;
+    if (aux == 'S'){
+        cout<<"Ingrese el correo electronico (usuario@dominio):"<<endl;
+        cout<<"\tIngrese el usuario: "; cin>>user;
+        cout<<"\tIngrese el dominio: "; cin>>domain;
+    }
+
+    leerCorreo(email,user,domain);
+    leerContacto(cambio,n,s,e,email);
+
+    cout << endl << "HA INGRESADO LA SIGUIENTE INFORMACION:" << endl;
+    imprimeContacto(cambio);
+    cout << endl << "Es correcta esta informacion? (S/N): ";
+    cin >> aux;
+    if (aux == 'N') {
+        modificaContacto(cambio);
+    } else {
+        system("pause");
+    }
+}  
+          
 void leerContacto(contactoEmail &c, string n, char s, int e, correo em){
     c.nom = n;
     c.sex = s;
