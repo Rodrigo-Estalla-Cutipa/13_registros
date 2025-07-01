67ee6c725878108fa4493b07f1ec1e9cfd7b81cc
Rodrigo-Estalla-Cutipa <restallac@unjbg.edu.pe>
2025-07-01 11:40:58 -0500

    Actualiza registro_3.cpp : agrega función comprobar fecha de nacimiento

--- registro_3.cpp
+++ registro_3.cpp
@@ -6,8 +6,10 @@ struct persona {
     int fn[3];
 };
 
+int comprobar(int, int, int);
+
 int main (){
-    int n, aux=0, mes=1;
+    int n, aux, mes;
     cout << "Ingrese la cantidad de personas:" << endl;
     cin >> n;
     persona lista[n];
@@ -19,6 +21,12 @@ int main (){
         getline(cin,lista[i].nom);
         cout << "Ingresa la fecha de nacimiento (dd mm aaaa): ";
         cin >> lista[i].fn[0] >> lista[i].fn[1] >> lista[i].fn[2];
+        aux = comprobar(lista[i].fn[0], lista[i].fn[1], lista[i].fn[2]);
+        while (aux == 0){
+            cout << "FECHA INVALIDA" << endl << "Ingrese nuevamente la fecha (dd mm aaaa): ";
+            cin >> lista[i].fn[0] >> lista[i].fn[1] >> lista[i].fn[2];
+            aux = comprobar(lista[i].fn[0], lista[i].fn[1], lista[i].fn[2]);
+        }
         cout << "\n";
     }
     
@@ -42,4 +50,38 @@ int main (){
 
     cout << "FIN DEL PROGRAMA";
     return 0;
+}
+
+int comprobar(int d, int m, int a){
+    int aux = 0;
+    switch(m){
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            if (d>0 && d<=31 && a>0 && a<2026){
+                aux = 1;
+            }
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            if (d>0 && d<=30 && a>0 && a<2026){
+                aux = 1;
+            }
+            break;
+        case 2:
+            if (a>0 && a<2026 && ((d>0 && d<=28) || (d == 29 && ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0))))){
+                aux = 1;
+            }
+            break;
+        default:
+            aux = 0;
+            break;
+    }
+    return aux;
 }
